c60b3f37ab9ae379e13e87f375d8db382efa9416
Jake Hansen <jakedhansen@outlook.com>
2021-01-18 15:09:16 -0600

    Add more documentation

--- schedulers/scheduler.go
+++ schedulers/scheduler.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 )
 
+// Scheduler manages scheduling and syncing Workers for a Job.
 type Scheduler struct {
 	dataChannel   chan interface{}
 	errorChannel  chan error
@@ -18,6 +19,8 @@ type Scheduler struct {
 	jobInProgress bool
 }
 
+// DefaultScheduler creates a Scheduler with a DataPrinterConsumer and ErrorPrinterConsumer. It also includes the
+// SequentialScheduler as the scheduling algorithm.
 func DefaultScheduler() *Scheduler {
 	scheduler := &Scheduler{
 		dataChannel:   nil,
@@ -31,6 +34,7 @@ func DefaultScheduler() *Scheduler {
 	return scheduler
 }
 
+// runWorker manages running a Worker and passes the Worker's return value and error to the appropriate channel.
 func runWorker(worker jobs.Worker, dataChannel chan interface{}, errorChannel chan error, wg *sync.WaitGroup) {
 	fmt.Printf("[DEBUG] starting worker %s\n", worker.WorkerName())
 	defer wg.Done()
@@ -41,16 +45,31 @@ func runWorker(worker jobs.Worker, dataChannel chan interface{}, errorChannel ch
 
 }
 
+// SchedulerAlgorithm defines an algorithm for scheduling Workers. An algorithm should return a pointer to a slice
+// with the given Workers in the wanted order. Workers will be started sequentially using this returned slice.
+//
+// For example, if the given Workers slice looks like this
+//
+//					[ w1, w2, w3, w4, w5 ]
+//
+// a reordered slice could be returned that looks like this
+//
+//					[ w5, w3, w1, w2, w4 ]
+//
+// Then, when the job that contains these Workers is scheduled, the Workers will be scheduled as w5-> w3-> w1-> w2-> w4.
 type SchedulerAlgorithm interface {
 	Schedule(workers *[]jobs.Worker) *[]jobs.Worker
 }
 
+// SequentialScheduler is a SchedulerAlgorithm that schedules Workers in the order in which they appear.
 type SequentialScheduler struct{}
 
+// Schedule returns an unmodified slice of the given Workers.
 func (s SequentialScheduler) Schedule(workers *[]jobs.Worker) *[]jobs.Worker {
 	return workers
 }
 
+// Schedule manages running a Job. When executed, Schedule begins spawning Workers as picked by the SchedulerAlgorithm.
 func (s *Scheduler) Schedule(job *jobs.Job) error {
 	if job != nil {
 		if !s.jobInProgress {
@@ -76,18 +95,21 @@ func (s *Scheduler) Schedule(job *jobs.Job) error {
 	return nil
 }
 
+// consumeData consumes the data channel for a Scheduler.
 func (s *Scheduler) consumeData() {
 	for val := range s.dataChannel {
 		s.DataConsumer.Consume(val)
 	}
 }
 
+// consumeErrors consumes the errors channel for a Scheduler.
 func (s *Scheduler) consumeErrors() {
 	for err := range s.errorChannel {
 		s.ErrorConsumer.Consume(err)
 	}
 }
 
+// cleanup waits for all Workers to finish before closing the data and error channels.
 func (s *Scheduler) cleanup() {
 	s.waitGroup.Wait()
 	close(s.dataChannel)
@@ -95,6 +117,7 @@ func (s *Scheduler) cleanup() {
 	s.jobInProgress = false
 }
 
+// WaitForWorkers blocks until all Workers have finished processing.
 func (s *Scheduler) WaitForWorkers() {
 	if s.waitGroup != nil {
 		s.waitGroup.Wait()
